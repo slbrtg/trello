ccbdc5961007b59f38dee3264999928bc3fcb948
slbrtg <slbrtg@gmail.com>
2018-04-10 16:33:38 -0700

    create jquery loop to display lists

--- static/js/script.js
+++ static/js/script.js
@@ -20,25 +20,106 @@ function loadLists() {
 }
 
 // Example code for displaying lists in the browser
+// function displayLists(lists) {
+//   // Lists should be ordered based on their 'pos' field
+//   lists.rows = _.sortBy(lists.rows, 'pos');
+//   lists.rows.forEach(function(list) {
+//     $('#board-title').text(list.name)
+//     var listDiv = $('<div>').addClass('hero is-primary');
+//     var listCardDiv = $('<div>').addClass('hero-body')
+//     var curElem = $('<header>').addClass('title').text(list.name);
+//     if (list.cards) {
+//       var innerUl = $('<ul>');
+//       list.cards.forEach(function(card) {
+//         innerUl.append($('<li>').text(card));
+//       });
+//       listCardDiv.append(curElem);
+//       listCardDiv.append(innerUl);
+//       listDiv.append(listCardDiv);
+//     }
+//     $('#board').append(listDiv);
+//   });
+// }
+
+
 function displayLists(lists) {
   // Lists should be ordered based on their 'pos' field
   lists.rows = _.sortBy(lists.rows, 'pos');
   lists.rows.forEach(function(list) {
-    var listDiv = $('<div>').addClass('hero is-primary');
-    var listCardDiv = $('<div>').addClass('hero-body')
-    var curElem = $('<header>').addClass('title').text(list.name);
-    if (list.cards) {
-      var innerUl = $('<ul>');
-      list.cards.forEach(function(card) {
-        innerUl.append($('<li>').text(card));
-      });
-      listCardDiv.append(curElem);
-      listCardDiv.append(innerUl);
-      listDiv.append(listCardDiv);
-    }
-    $('#board').append(listDiv);
+    $('#main-content').append(
+
+    '<section class="hero is-primary is-medium">' +
+      '<div class="hero-head">' +
+        '<nav class="navbar">' +
+          '<div class="container">' +
+            '<div class="navbar-brand">' +
+              '<a class="navbar-item">' +
+                '<h1 id="board-title" class="title">' + list.name + '</h1>' +
+              '</a>' +
+              '<span id="burger-menu" class="navbar-burger burger" data-target="navbarMenuHeroA">' +
+                '<span></span>' +
+                '<span></span>' +
+                '<span></span>' +
+              '</span>' +
+            '</div>' +
+            '<div id="navbarMenuHeroA" class="navbar-menu">' +
+              '<div class="navbar-end">' +
+                '<a class="navbar-item is-active">' +
+                  'Home' +
+                '</a>' +
+                '<a class="navbar-item">' +
+                  'Examples' +
+                '</a>' +
+                '<a class="navbar-item">' +
+                  'Documentation' +
+                '</a>' +
+                '<span class="navbar-item">' +
+                  '<a class="button is-primary is-inverted">' +
+                    '<span class="icon">' +
+                      '<i class="fab fa-github"></i>' +
+                   ' </span>'+
+                    '<span>Download</span>' +
+                  '</a>' +
+                '</span>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</nav>' +
+      '</div>' +
+
+      
+        '<div class="hero-body">' +
+        '<div class="container has-text-centered">' +
+          '<h1 class="title">' +
+            'Title' +
+          '</h1>' +
+          '<h2 class="subtitle">' +
+            'Subtitle' +
+          '</h2>' +
+        '</div>' +
+      '</div>' +
+
+
+      '<div class="hero-foot">' +
+        '<nav class="tabs">' +
+          '<div class="container">' +
+            '<ul>' +
+              '<li class="is-active"><a>Overview</a></li>' +
+              '<li><a>Modifiers</a></li>' +
+              '<li><a>Grid</a></li>' +
+              '<li><a>Elements</a></li>' +
+              '<li><a>Components</a></li>' +
+              '<li><a>Layout</a></li>' +
+            '</ul>' +
+          '</div>' +
+        '</nav>' +
+      '</div>' +
+    '</section>'
+    )
   });
 }
+
+
 
 loadLists()
   .then(function(data) {
